3bdcfd7b092df67e4f99b352471da8e4c46016a7
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-22 16:39:57 +0100

    Update simpleLightBox.js

--- src/js/simpleLightBox.js
+++ src/js/simpleLightBox.js
@@ -1,20 +1,20 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-export default function showGallary() {
-    const gallery = document.querySelector('.gallery');
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// export default function showGallary() {
+//     const gallery = document.querySelector('.gallery');
 
-    gallery.addEventListener('click', onClickGallery);
+//     gallery.addEventListener('click', onClickGallery);
 
-    function onClickGallery(event) {
-        event.preventDefault();
+//     function onClickGallery(event) {
+//         event.preventDefault();
 
-        new SimpleLightbox('.gallery a', { 
-        overlayOpacity: 0.8,
-        captionType: 'att',
-        captionsData: 'alt',
-    });
-    }
-}
+//         new SimpleLightbox('.gallery a', { 
+//         overlayOpacity: 0.8,
+//         captionType: 'att',
+//         captionsData: 'alt',
+//     });
+//     }
+// }
 // export {
 //     showGallary
 //  }
