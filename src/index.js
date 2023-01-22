19b671933c15ba000b199e691ec7116859b1e978
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-22 17:07:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // import apiServise from './js/api';
 // import createMarkup from './js/markupService';
 import main from './js/main';
-import showGallary from './js/simpleLightBox'
+// import showGallary from './js/simpleLightBox'
